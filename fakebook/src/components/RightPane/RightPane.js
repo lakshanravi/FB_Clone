688646e4c744ea47120fb80f438a6ba681bcffdb
lakshanravi <rgrlranathunga@std.appsc.sab.ac.lk>
2025-03-25 00:12:00 +0530

    final development of the clss

--- fakebook/src/components/RightPane/RightPane.js
+++ fakebook/src/components/RightPane/RightPane.js
@@ -16,7 +16,43 @@ function RightPane() {
           <img src="images/4.jpeg" alt="" className="birtthdayImg" />
           <span className="birthdaytext"><b>Kumar Sangakkara</b> and <b>2 others</b>having birthday today..!</span>
         </div>
+        <div className="friendList">
+          <li className="onlineFriend">
+            <div className="onlineFriendContainer">
+              <img src="images/5.jpg" alt="" className="onlineFriendImg" />
+              <span className="onlineStatus"></span>
+           
+            </div>
+            <span className="onlinefriendName">Michel Arthor</span>
+          </li>
 
+          <li className="onlineFriend">
+            <div className="onlineFriendContainer">
+              <img src="images/5.jpg" alt="" className="onlineFriendImg" />
+              <span className="onlineStatus"></span>
+           
+            </div>
+            <span className="onlinefriendName">Michel Arthor</span>
+          </li>
+
+          <li className="onlineFriend">
+            <div className="onlineFriendContainer">
+              <img src="images/5.jpg" alt="" className="onlineFriendImg" />
+              <span className="onlineStatus"></span>
+           
+            </div>
+            <span className="onlinefriendName">Michel Arthor</span>
+          </li>
+          <li className="onlineFriend">
+            <div className="onlineFriendContainer">
+              <img src="images/5.jpg" alt="" className="onlineFriendImg" />
+              <span className="onlineStatus"></span>
+           
+            </div>
+            <span className="onlinefriendName">Michel Arthor</span>
+          </li>
+          
+        </div>
       </div>
     </div>
   )
